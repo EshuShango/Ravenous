2ffc1eaa2482c4d6f26fe19014d6f50971064a1d
EshuShango <tobiakinyemi21@gmail.com>
2018-01-29 13:14:26 -0600

    up2

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,8 +1,6 @@
 const apiKey = 'a3yN1ipY8lYzT9AZr8MlXksvfR278dZyeebOJvv92wduPgml526P8gbx4LofvKljNa4191p7HVZ_W7TjD8_CG1WsLLKjVdlNNG0rKYQE3CPPoPAgeOj6VepojUZqWnYx';
- const yelp = {
+  const yelp = {
    search(term,location,sortBy) {
-
-
           return fetch(` https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy} `,{
            headers: {
        Authorization: `Bearer ${apiKey}`
