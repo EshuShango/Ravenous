59c56d8291b44e4e49c154b1e9561e41be4d57da
EshuShango <tobiakinyemi21@gmail.com>
2018-01-29 12:26:34 -0600

    hello

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,16 +1,15 @@
 const apiKey = 'a3yN1ipY8lYzT9AZr8MlXksvfR278dZyeebOJvv92wduPgml526P8gbx4LofvKljNa4191p7HVZ_W7TjD8_CG1WsLLKjVdlNNG0rKYQE3CPPoPAgeOj6VepojUZqWnYx';
  const yelp = {
    search(term,location,sortBy) {
-      return fetch(`./src/util/Yelp.js
-Syntax error: Unexpected token (6:6)
 
-  4 |       return fetch( `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy},` {
-  5 |         headers: {
-> 6 |       Authorization: `Bearer ${apiKey}`
-    |       ^
-  7 |     }
-  8 |   }).then(response => {
-  9 |     return response.json();`,{
+
+          return fetch(` https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy} `,{
+           headers: {
+       Authorization: `Bearer ${apiKey}`
+
+     }
+   }).then(response => {
+      return response.json();`,{
         headers: {
       { Authorization: `Bearer ${apiKey}` }
     }
