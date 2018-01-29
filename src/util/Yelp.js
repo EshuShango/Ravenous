088ec4f9cb34b51b1d08b008f6d7b302b4171987
EshuShango <tobiakinyemi21@gmail.com>
2018-01-29 11:40:43 -0600

    hey

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'a3yN1ipY8lYzT9AZr8MlXksvfR278dZyeebOJvv92wduPgml526P8gbx4LofvKljNa4191p7HVZ_W7TjD8_CG1WsLLKjVdlNNG0rKYQE3CPPoPAgeOj6VepojUZqWnYx';
+ const yelp = {
+   search(term,location,sortBy) {
+      return fetch(`https://https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+        headers: {
+      {Authorization: `Bearer ${apiKey}`} 
+    }
+  }).then(response => {
+    return response.json();
+  }).then(jsonResponse => {
+    if(jsonResponse.businesses){
+      return jsonResponse.businesses.map(business => {
+        id: business.id,
+        imageSrc: business.image_url,
+        name: business.name,
+        address: business.address,
+        city: business.city,
+        state: business.state ,
+        zipCode: business.zipCode,
+        category: business.category ,
+        rating: business.rating,
+        reviewCount: business.reviewCount
+      });
+    }
+  });
+    }
+ };
+
+
+export default Yelp;
